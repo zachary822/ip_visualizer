46c7c8920c74ccc2bde4cc5b2748fdd87decc046
Zachary Juang <zachary822@me.com>
2022-09-12 00:36:57 -0400

    Proper mui ssr integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,33 @@
-import type { AppProps } from "next/app";
-import Head from "next/head";
-import { useState } from "react";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
 import {
   Hydrate,
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { useState } from "react";
 import "../styles/globals.css";
+import createEmotionCache from "../utils/createEmotionCache";
+import theme from "../utils/theme";
 
-function MyApp({ Component, pageProps }: AppProps<any>) {
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps<any> {
+  emotionCache?: EmotionCache;
+}
+
+function MyApp({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}: MyAppProps) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <>
+    <CacheProvider value={emotionCache}>
       <Head>
         <title>IP Visualizer</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
@@ -21,10 +36,13 @@ function MyApp({ Component, pageProps }: AppProps<any>) {
       </Head>
       <QueryClientProvider client={queryClient}>
         <Hydrate state={pageProps.dehydratedState}>
-          <Component {...pageProps} />
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+          </ThemeProvider>
         </Hydrate>
       </QueryClientProvider>
-    </>
+    </CacheProvider>
   );
 }
 
